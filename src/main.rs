2175e7d7b40060fe8df6e26b8a124ec3925bb498
frolleks <frolleks@hotmail.com>
2023-03-03 20:57:05 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn main() {
+    let mut x = String::new();
+    let mut y = String::new();
+    let mut operator = String::new();
+
+    println!("Please enter the first number");
+    io::stdin().read_line(&mut x).expect("Failed to read line");
+    println!("Please enter the second number");
+    io::stdin().read_line(&mut y).expect("Failed to read line");
+    println!("Please enter an operator (e.g. multiply, add, subtract, divide)");
+    io::stdin()
+        .read_line(&mut operator)
+        .expect("Failed to read line");
+
+    let x: u32 = x.trim().parse().expect("Please type a number!");
+    let y: u32 = y.trim().parse().expect("Please type a number!");
+    let _operator: String = operator.trim().parse().expect("Please enter an operator!");
+
+    if &_operator == "add" {
+        println!("Answer: {}", x + y);
+    } else if &_operator == "subtract" {
+        println!("Answer: {}", x - y);
+    } else if &_operator == "multiply" {
+        println!("Answer: {}", x - y);
+    } else if &_operator == "divide" {
+        println!("Answer: {}", x / y);
+    }
+}
